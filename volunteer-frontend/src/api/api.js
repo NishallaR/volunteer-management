0efa0866269b6856ae37de97716f8367c2ec4ce4
NishallaR <nishallaramkhalawan@gmail.com>
2026-01-02 11:47:35 -0400

    Your commit message

--- volunteer-frontend/src/api/api.js
+++ volunteer-frontend/src/api/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// This will use your live Render URL. 
+// Tip: In the future, you can use process.env.VITE_API_URL for even more flexibility!
+const API_BASE_URL = "https://volunteer-management.onrender.com/api";
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: API_BASE_URL,
 });
 
 // Attach token automatically
